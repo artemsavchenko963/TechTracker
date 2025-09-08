8d0582c9b42d0fba84f477368a75b87c629b0b30
Artem Savchenko <artemsavcenko963@gmail.com>
2025-09-08 20:40:00 +0200

    Add percentage on JS

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector('#modal')
 const content = document.querySelector('#content')
 const backdrop = document.querySelector('#backdrop')
+const progress = document.querySelector('#progress')
 
 content.addEventListener('click', openCard)
 backdrop.addEventListener('click', closeModal)
@@ -8,9 +9,9 @@ backdrop.addEventListener('click', closeModal)
 const technologies = [
     { title: 'HTML', description: 'HTML text', type: 'html', done: true },
     { title: 'CSS', description: 'CSS text', type: 'css', done: true },
-    { title: 'Java Script', description: 'Java Script text', type: 'js', done: false },
-    { title: 'Git', description: 'Git text', type: 'git', done: false },
-    { title: 'React', description: 'React text', type: 'react', done: false }
+    { title: 'Java Script', description: 'Java Script text', type: 'js', done: true },
+    { title: 'Git', description: 'Git text', type: 'git', done: true },
+    { title: 'React', description: 'React text', type: 'react', done: false },
 ]
 
 console.log(technologies);
@@ -24,6 +25,11 @@ function closeModal() {
 }
 
 function init() {
+    renderCards()
+    renderProgress()
+}
+
+function renderCards() {
     if (technologies.length === 0) {
         content.innerHTML = `<p class="empty">There are nothing...</p>`
     } else {
@@ -36,9 +42,38 @@ function init() {
     }
 }
 
+function renderProgress() {
+    const percent = computeProgressPercent()
+
+    let background
+
+    if (percent <= 30) {
+        background = 'red'
+    } else if (percent > 30 && percent < 70) {
+        background = '#ff6a00'
+    } else {
+        console.log('biibibibi')
+        background = '#73BA3C'
+    }
+
+    progress.style.background = background
+    progress.style.width = percent + '%'
+    progress.textContent = percent ? percent + '%' : ''  //----!!!!!!------------------------------------------------
+}
+
+function computeProgressPercent() {
+    if (technologies.length === 0) {
+        return 0
+    }
+    let doneCount = 0
+    for (let i = 0; i < technologies.length; i++) {
+        if (technologies[i].done === true) doneCount++
+    }
+    return Math.round((100 * doneCount) / technologies.length)
+}
+
 function toCard(tech) {
     const doneClass = tech.done ? 'done' : ''
-
     return `
         <div class="card ${doneClass}">
             <h3>${tech.title}</h3>
