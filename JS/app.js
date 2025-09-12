912e1098c33162ee445d980afa3643ebe2724a04
Artem Savchenko <artemsavcenko963@gmail.com>
2025-09-12 23:00:06 +0200

    Add optimization

--- JS/app.js
+++ JS/app.js
@@ -3,14 +3,17 @@ const content = document.querySelector('#content')
 const backdrop = document.querySelector('#backdrop')
 const progress = document.querySelector('#progress')
 
+const APP_TITLE = document.title
+
 content.addEventListener('click', openCard)
 backdrop.addEventListener('click', closeModal)
+modal.addEventListener('change', toggleTech)
 
 const technologies = [
     { title: 'HTML', description: 'HTML text', type: 'html', done: true },
     { title: 'CSS', description: 'CSS text', type: 'css', done: true },
-    { title: 'Java Script', description: 'Java Script text', type: 'js', done: true },
-    { title: 'Git', description: 'Git text', type: 'git', done: true },
+    { title: 'Java Script', description: 'Java Script text', type: 'js', done: false },
+    { title: 'Git', description: 'Git text', type: 'git', done: false },
     { title: 'React', description: 'React text', type: 'react', done: false },
 ]
 
@@ -18,9 +21,42 @@ console.log(technologies);
 
 
 function openCard() {
+    const data = event.target.dataset
+    const tech = technologies.find(t => t.type === data.type)
+    if (!tech) return
+
+    openModal(toModal(tech), tech.title)
+}
+
+function toModal(tech) {
+    const checked = tech.done ? 'checked' : ''
+    return `
+        <h2>${tech.title}</h2>
+        <p>${tech.description}</p>
+        <hr>
+        <div>
+            <input type="checkbox" id="done" ${checked} data-type="${tech.type}">
+            <label for="done">Done!</label>
+        </div>
+    `
+}
+
+function toggleTech(event) {
+    const type = event.target.dataset.type
+    const tech = technologies.find(t => t.type === type)
+    tech.done = event.target.checked
+
+    init()
+}
+
+function openModal(html, title = APP_TITLE) {
+    document.title = `${title} | ${APP_TITLE}`
+    modal.innerHTML = html
     modal.classList.add('open')
+
 }
 function closeModal() {
+    document.title = APP_TITLE
     modal.classList.remove('open')
 }
 
@@ -52,7 +88,6 @@ function renderProgress() {
     } else if (percent > 30 && percent < 70) {
         background = '#ff6a00'
     } else {
-        console.log('biibibibi')
         background = '#73BA3C'
     }
 
@@ -75,8 +110,8 @@ function computeProgressPercent() {
 function toCard(tech) {
     const doneClass = tech.done ? 'done' : ''
     return `
-        <div class="card ${doneClass}">
-            <h3>${tech.title}</h3>
+        <div class="card ${doneClass}" data-type="${tech.type}">
+            <h3 data-type="${tech.type}">${tech.title}</h3>
         </div>
     `
 }
